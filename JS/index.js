4dc92c1acf5c38fe110cc3a0772b583332d7b271
Anne <anne.dupin91@gmail.com>
2021-04-21 11:18:03 +0200

    First

---
+++ JS/index.js
@@ -0,0 +1,29 @@
+//let nombre = 1;
+
+//while (true) { // while = boucle = tant que la condition entre parenthèses est vérifiée, la boucle va exécuter code autant de fois que nécessaire
+    //console.log(nombre); 
+
+    //if(nombre == 32) {
+     //   break;
+   // }
+   // nombre++; // = nombre + 1 (incrémenter pour être = à 32)
+//}
+
+// Deux façons de faire 
+
+let thisArray = ['Vanille', 'Fraise', 'Chocolat'];
+console.log(thisArray);
+//let thisArray = new Array('Vanille', 'Fraise', 'Chocolat', 'Caramel');
+
+//Une boucle simple qui incrémente un indice et accède aux éléments successifs par cet indice.
+
+// Avec For()
+
+for (let i = 0; i < thisArray.length; i++)
+{
+    alert(thisArray[i]);
+}
+// Methode la + rapide
+
+
+
